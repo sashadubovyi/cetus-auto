d1814792885c58c2b3b8b30766d5be4a33c2b502
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-10-31 16:43:59 +0200

    files

--- js/main.js
+++ js/main.js
@@ -1,2 +1,63 @@
+/////////////////////
+/////servise list
+/////////////////////
+
 const serviceList = document.querySelector(".service-list");
-const ListBtn = 
+const listBtn = document.querySelector(".our-service-btn");
+const svg = document.querySelector(".service-svg-container");
+
+let isOpenList = false;
+listBtn.addEventListener("click", openList);
+
+function openList() {
+  if (!isOpenList) {
+    isOpenList = true;
+    serviceList.style.display = "grid";
+    svg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="svg-arrow" width="24" height="24"><path d="m12 6.586-8.707 8.707 1.414 1.414L12 9.414l7.293 7.293 1.414-1.414L12 6.586z"/></svg>`;
+  } else {
+    isOpenList = false;
+    serviceList.style.display = "none";
+    svg.innerHTML = `<svg
+              class="svg-arrow"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+            >
+              <path
+                d="M12 17.414 3.293 8.707l1.414-1.414L12 14.586l7.293-7.293 1.414 1.414L12 17.414z"
+              />
+            </svg>`;
+  }
+}
+
+/////////////////////
+/////contacts list
+/////////////////////
+
+const contactsList = document.querySelector(".contacts-container");
+const contactsOpenBtn = document.querySelector(".contacts-btn");
+const arrowContacts = document.querySelector(".contacts-svg-container");
+
+let isOpenContacts = false;
+contactsOpenBtn.addEventListener("click", openContacts);
+
+function openContacts() {
+  if (!isOpenContacts) {
+    isOpenContacts = true;
+    contactsList.style.display = "flex";
+    arrowContacts.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="svg-arrow" width="24" height="24"><path d="m12 6.586-8.707 8.707 1.414 1.414L12 9.414l7.293 7.293 1.414-1.414L12 6.586z"/></svg>`;
+  } else {
+    isOpenContacts = false;
+    contactsList.style.display = "none";
+    arrowContacts.innerHTML = `<svg
+              class="svg-arrow"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+            >
+              <path
+                d="M12 17.414 3.293 8.707l1.414-1.414L12 14.586l7.293-7.293 1.414 1.414L12 17.414z"
+              />
+            </svg>`;
+  }
+}
